5cca0ae9dca9c4bd59707befb2327b86969e8df6
adi-ti1 <119519496+adi-ti1@users.noreply.github.com>
2024-04-23 09:39:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path =require('path');
+require ('dotenv').config();
+const app =express();
+const PORT = 1245;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'public folder'))); 
+const mongoURI=process.env.MONGO_URI;
+//connect to MongoDB
+mongoose.connect(mongoURI)
+.then(()=> console.log('Connected to MongoDB'))
+.catch(err => console.error('error connecting to mongoDB : ',err));
+
+///user schema
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    password : String
+});
+const User = mongoose.model('User',userSchema);
+
+app.get('/users',(req,res)=>{
+    User.find({})
+    .then(users => res.json(users))
+    .catch(err => res.status(500).json({ message: err.message}));
+});
+app.post('/users', (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user.save()
+        .then(newUser => res.status(201).json(newUser)) // Respond with the created user
+        .catch(err => res.status(400).json({ message: err.message }));
+});
+app.put('/users/:id',(req,res)=>{
+    const userId = req.params.id;
+    const updateData ={
+        name: req.body.name,
+        email: req.body.email,
+        password:req.body.password 
+    };
+    User.findByIdAndUpdate(userId,updateData,{new: true})
+    .then(updatedUser => {
+        if(!updatedUser){
+            return res.status(404).json({message: 'User not found'});
+        }
+        res.json(updatedUser);
+    })
+    .catch(err => res.status(400).json({message : err.message}));
+});
+app.delete('/users/:id',(req,res) =>{
+    const userId = req.params.id;
+    User.findByIdAndDelete(userId)
+    .then(deletedUser => {
+        if(!deletedUser){
+            return res.status(404).json({message:'User not found'});
+        }
+        res.json({message:'User deleted successfully'});
+    })
+    .catch(err => res.status(400).json({message : err.message}));
+});
+
+app.listen(PORT);
